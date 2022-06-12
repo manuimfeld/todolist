2567997e8721c790e0cf9dd59fa4113a52ea40ec
manuimfeld <manu_jadir@hotmail.com>
2022-06-12 17:37:01 -0300

    Todolist V1.1 / Cambios en el diseño, responsive completo y algun cambio en el script

--- script.js
+++ script.js
@@ -3,92 +3,93 @@ let form = document.getElementById('form')
 let ul = document.getElementById('ul')
 let tareasContenedor = []
 
+/* Con esta función muestro el numero de tareas existentes en el array */
+function todocounter() {
+    return document.getElementById('todo-number').innerText = `Hay ${tareasContenedor.length} tareas pendientes`
+}
 
-/*** FUNCIONES AL CARGAR LA PÁGINA ***/
-/* Con esta función convierto el item del localStorage de Strin a Array y lo pusheo a la let tareasContenedor */
+/* Con esta función agrego las tareas existentes en el array al LocalStorage */
+let setTodo = (e) => {
+    localStorage.setItem("tareas", [JSON.stringify(e)])
+}
+
+/* Con esta función recorro el localStorage y lo convierto a Array, despues lo pusheo a tareasContenedor */
 let guardado = () => {
     let conver = JSON.parse(localStorage.getItem("tareas"))
     
-    if (conver != null) {
+    if (conver.length > 0) {
         conver.map(tarea => {
             tareasContenedor.push(tarea)
         })
+        todocounter()
     } 
 }
 
-/* Con esta función muestro los elementos que tengo en la let tareasContenedor, agregando un li en el body del html*/
-let mostrarTareas = (e) => {
+/* Con esta función recorro el array de tareas y los agrego a la página */
+let showTodo = (e) => {
     e.map(tareaNueva => {
         let newLi = document.createElement('li')
         let newDiv = document.createElement('div')
+
+        let newInputCheck = document.createElement('input')
+        newInputCheck.setAttribute('type', 'checkbox')
 
         let newP = document.createElement('p')
         let newPcont = document.createTextNode(tareaNueva)
         newP.appendChild(newPcont)
 
-        let tareaFinalizada = document.createElement('button')
-        let tareaFcont = document.createTextNode('Finalizar')
-        tareaFinalizada.appendChild(tareaFcont)
-        tareaFinalizada.style.background = "#2578bb"
-
         let newB = document.createElement('button')
         let newBcont = document.createTextNode('Borrar')
         newB.appendChild(newBcont)
 
+        newLi.appendChild(newInputCheck)
         newLi.appendChild(newP)
-        newDiv.appendChild(tareaFinalizada)
+
         newDiv.appendChild(newB)
         newDiv.className = 'tarea-opcion'
+
         newLi.appendChild(newDiv)
         ul.appendChild(newLi)
 
-        tareaFinalizada.addEventListener('click', () => {
-            newP.style.textDecoration = "line-through";
-        })
-
         newB.addEventListener("click", function (e) {
-
             /* Busco el indice del elemento seleccionado y despues lo uso para borrarlo en el localStorage*/
             let borrar = newP.textContent
             let index = tareasContenedor.indexOf(borrar);
             tareasContenedor.splice(index, 1);
-            localStorage.setItem("tareas", JSON.stringify(tareasContenedor))
+            setTodo(tareasContenedor)
 
-            /* Elimino el elemento li clickeado. */
+            /* Elimino el elemento li clickeado y actualizo las tareas pendientes */
             newDiv.parentNode.parentNode.removeChild(newDiv.parentNode);
+            todocounter()
         });
     })
 }
 
-/* llamo a las funciones cuando carga la página */
+/* llamo a las funciones cuando carga la página 
+
+verifico que haya tareas en el LocalStorage */
 guardado()
-mostrarTareas(tareasContenedor)
+/* Agrego las tareas que hay en el LocalStorage a la página */
+showTodo(tareasContenedor)
 
-
-/*** FUNCIONES ***/
-let setTarea = (e) => {
-    localStorage.setItem("tareas", [JSON.stringify(e)])
-}
-
-
-/*** EVENTOS ***/
-
-/* Agarro el valor del input, lo pusheo a la let tareasContenedor, convierto esa let en json y lo guardo en localStorage */
+/*** EVENTO DEL FORMULARIO PARA AGREGAR TAREAS 
+ * 
+Agarro el valor del input, lo pusheo a la let tareasContenedor, convierto esa let en json y lo guardo en localStorage */
 form.addEventListener('submit',  (e) => {
     e.preventDefault()
     let tarea = document.getElementById('tarea').value
 
     if (tarea.length > 0) {
         tareasContenedor.push(tarea)
-        setTarea(tareasContenedor)
+        setTodo(tareasContenedor)
+        todocounter()
         form.reset()
 
         if (tareasContenedor.length > 0) {
             /* Si el array contiene elementos, voy a crear y mostrar el ultimo elemento del array en pantalla */
-            return mostrarTareas(tareasContenedor.slice(-1))
+            return showTodo(tareasContenedor.slice(-1))
         }
     } else {
         alert('No puedes agregar una tarea vacía, por favor escribe una tarea.')
     }
 })
-
